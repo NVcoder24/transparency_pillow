ba0b001089742fbafaa696309d3815a21b924fda
Coder <60108915+NVcoder24@users.noreply.github.com>
2021-02-18 21:59:37 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+cfg = {
+    "input_file_name":"input.png",     # input file
+    "output_file_name":"output.png",   # output file
+    "opacity":0,                       # opacity (0 - 100%)
+    "color":(0, 0, 0),                 # (r, g, b) | white (255, 255, 255) | black (0, 0, 0)
+    "show_image":False                 # show image after saving it
+}
